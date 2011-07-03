05bdb70577259d16cae1fd4b9c998744961e8ca0
miron <miron@wlkp.de>
2011-07-03 16:56:33 +0200

    added some the selection menu

--- main.cpp
+++ main.cpp
@@ -3,7 +3,22 @@
 using namespace std;
 
 int main(void){
+	int selection;
 
+	cout << "1. Decode " << endl;
+	cout << "2. Encode " << endl;
+
+	cin >> selection;
+	
+	// decode
+	if(selection == 1){
+	
+	}
+	
+	// encode
+	if(selection == 2){
+	
+	}
 
 	return 0;
 }
